8c661199e2f6e34163834f0f45e00ad4719dc5eb
Aadit Aggarwal <aadit.asr5904@gmail.com>
2025-07-22 07:18:54 +0530

    Updated for local_llm

--- local_llm.py
+++ local_llm.py
@@ -7,3 +7,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# create iniitializing prompt
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. You are funny too and try to lighten the mood."),
+        ("user", "Question: {question}"),
+    ]
+)
+
+# streamlit
+
+st.title("Local LLM with Ollama")
+input_text = st.text_input("How can I help you today?")
+
+# Ollama
+llm = Ollama(model="llama2", temperature=1.1)
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
+
+if input_text:
+    st.write(chain.invoke({"question": input_text}))
